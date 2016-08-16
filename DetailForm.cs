d22dfcd7ccb2b48dc55d93ff47953aa640396ea4
sean <305120262@qq.com>
2016-08-16 10:09:36 +0800

    ok

---
+++ DetailForm.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ArcMapShowGridAddin
+{
+    public partial class DetailForm : Form
+    {
+        double x_source;
+        double y_source;
+
+        public DetailForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double xmap,ymap;
+            if(Convert2Decimal(this.textBox1.Text,this.textBox2.Text,this.textBox3.Text,out xmap)&&Convert2Decimal(this.textBox4.Text,this.textBox5.Text,this.textBox6.Text,out ymap)){
+                var pointpair = new string [4];
+            pointpair[0] = x_source.ToString();
+            pointpair[1] = y_source.ToString();
+            pointpair[2] = xmap.ToString();
+            pointpair[3] = ymap.ToString();
+            gvPoints.Rows.Add(pointpair);
+            }
+            
+        }
+
+        public void AddPoints(double x, double y)
+        {
+            x_source = Math.Round(x,3);
+            y_source = Math.Round(y,3);
+            this.lblXSource.Text = x_source.ToString();
+            this.lblYSource.Text = y_source.ToString();
+        }
+
+        bool Convert2Decimal(string degree, string minute, string second,out double result)
+        {
+            double d,m,s;
+            if(double.TryParse(degree,out d) && double.TryParse(minute,out m) && double.TryParse(second,out s))
+            {
+                result = Math.Round((d + m / 60 + s / 60), 3) ;
+                return true;
+            }else
+            {
+                result = 0;
+                return false;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            gvPoints.Rows.Clear();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dlg = new SaveFileDialog();
+            dlg.Filter = "txt|*.txt";
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                Stream file = dlg.OpenFile();
+                StreamWriter writer = new StreamWriter(file);
+                foreach (DataGridViewRow row in gvPoints.Rows)
+                {
+                    string[] para= new string[4];
+                    para[0] = (string)row.Cells[0].Value;
+                    para[1] = (string)row.Cells[1].Value;
+                    para[2] = (string)row.Cells[2].Value;
+                    para[3] = (string)row.Cells[3].Value;
+                    writer.WriteLine("{0} {1} {2} {3}", para);
+                    
+                }
+                writer.Close();
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("ArcMapShowGridAddin")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ArcMapShowGridAddin")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("18261592-7bee-43df-a5d3-bf9c4a99e638")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
